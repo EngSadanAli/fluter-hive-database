7df8f9da4b2a12c8cdf1e055acf7483aff371503
Syed Sadan Ali <lifeincodes10101@gmail.com>
2023-01-26 17:16:52 +0500

    Second Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,12 +13,53 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Center(child: Text('Hive DataBase'))),
-      body: Column(children: []),
+      body: Column(children: [
+        FutureBuilder(
+            future: Hive.openBox('sadan'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(snapshot.data!.get('name').toString()),
+                      trailing: IconButton(
+                        onPressed: () {
+                          setState(() {});
+                          snapshot.data!.put('name', 'Congratulations');
+                        },
+                        icon: Icon(Icons.edit),
+                      ),
+                      subtitle: Text(snapshot.data!.get('age').toString()),
+                    )
+                  ],
+                );
+              } else {
+                return Text('Loading...');
+              }
+            }),
+        FutureBuilder(
+            future: Hive.openBox('waleed'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(snapshot.data!.get('name').toString()),
+                    )
+                  ],
+                );
+              } else {
+                return Text('Loading...');
+              }
+            }),
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           var box = await Hive.openBox('sadan');
+          var box1 = await Hive.openBox('waleed');
 
-          box.put('name', 'sadan');
+          box1.put('name', 'Waleed Khan');
+          box.put('name', 'sadan ali');
           box.put('age', 24);
 
           box.put('details', {
